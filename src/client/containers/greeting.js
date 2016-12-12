99d8e05b84808f70a38341b51b5f950d66aa289c
Armand (Tydax) Bour <tydaxx@gmail.com>
2016-12-12 17:43:49 +0100

    Change redux state manipulations to Immutable obj

--- src/client/containers/greeting.js
+++ src/client/containers/greeting.js
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 import Message from '../components/message';
 
 const mapStateToProps = state => ({
-  message: state.greeting.saidHello ? 'Hello World!' : 'I did not say hello.',
+  message: state.greeting.get('saidHello') ? 'Hello World!' : 'I did not say hello.',
 });
 
 export default connect(mapStateToProps)(Message);
