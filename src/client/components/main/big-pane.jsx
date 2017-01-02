056b9f200d9071f2694cf20d17e918339b08272b
Armand (Tydax) Bour <tydaxx@gmail.com>
2017-01-02 17:08:27 +0100

    Add gold ratio pane components

---
+++ src/client/components/main/big-pane.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const BigPane = ({ children }) => (
+  <div className='big-pane'>
+    {children}
+  </div>
+);
+
+export default BigPane;
---
+++ src/client/components/main/small-pane.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SmallPane = ({ children }) => (
+  <div className='small-pane'>
+    {children}
+  </div>
+);
+
+export default SmallPane;
