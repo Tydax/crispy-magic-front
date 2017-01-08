7f5b9016414abd3a252b9596a402575da2e09115
Armand (Tydax) Bour <tydaxx@gmail.com>
2017-01-08 16:00:38 +0100

    Remove Hello world React and Redux files

--- src/client/components/message.jsx
+++
@@ -1,9 +0,0 @@
-import React, { PropTypes } from 'react';
-
-const Message = ({ message }) => <h2>{message}</h2>;
-
-Message.propTypes = {
-  message: PropTypes.string.isRequired,
-};
-
-export default Message;
