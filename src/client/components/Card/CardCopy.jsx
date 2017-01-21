fb0ba57f04ece9e6da78e03bbb63c30e23223df5
Armand (Tydax) Bour <tydaxx@gmail.com>
2017-01-21 23:38:45 +0100

    Reformat CardCopy component file

--- src/client/components/Card/CardCopy.jsx
+++ src/client/components/Card/CardCopy.jsx
@@ -3,14 +3,20 @@ import React from 'react';
 import { capitalise } from '../../../shared/utils';
 import { CardPropTypes } from '../../../shared/prop-types';
 
-
-const CardCopy = function CardCopy({ numberCopies, rarity }) {
-  return <span className={`nbcopies mtg${capitalise(rarity)}`}>{numberCopies}</span>;
-};
-
-CardCopy.propTypes = {
-  numberCopies: CardPropTypes.numberCopies.isRequired,
+const propTypes = {
+  numberCopies: CardPropTypes.numberCopies,
   rarity: CardPropTypes.rarity.isRequired,
 };
+
+const defaultProps = {
+  numberCopies: 1,
+};
+
+const CardCopy = function CardCopy({ numberCopies, rarity }) {
+  return <span className={`nbcopies -${rarity.toLowerCase()}`}>{numberCopies}</span>;
+};
+
+CardCopy.propTypes = propTypes;
+CardCopy.defaultProps = defaultProps;
 
 export default CardCopy;
